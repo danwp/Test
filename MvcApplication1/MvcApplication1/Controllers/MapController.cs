920d6d91ea3140cdf68ee74c8316512d298d390e
danwp <danwp1296@hotmail.com>
2013-11-19 00:03:00 +0000

    Test Google Maps

---
+++ MvcApplication1/MvcApplication1/Controllers/MapController.cs
@@ -0,0 +1,23 @@
+﻿using MvcApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MvcApplication1.Controllers
+{
+    public class MapController : Controller
+    {
+        //
+        // GET: /Map/
+
+        public ActionResult Index(string postcode)
+        {
+            var mv = new PostcodeModel { Postcode = postcode };
+            return View(mv);
+        }
+
+    }
+}
